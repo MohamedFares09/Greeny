47f59bdc43d1a3b0a955420b70d07070a6efba9a
Mohamed Fares <mofares010@gmail.com>
2026-03-03 04:14:43 +0200

    add review entity

---
+++ lib/core/entities/review_entity.dart
@@ -0,0 +1,15 @@
+class ReviewEntity {
+  final String name;
+  final String date;
+  final String image;
+  final int rating;
+  final String reviewDescription;
+
+  ReviewEntity({
+    required this.name,
+    required this.date,
+    required this.image,
+    required this.rating,
+    required this.reviewDescription,
+  });
+}
