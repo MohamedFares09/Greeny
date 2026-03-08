8bc3b2723b2237735b096230bd943bb77f58770b
Mohamed Fares <mofares010@gmail.com>
2026-03-08 22:02:48 +0200

    product repo impl

--- lib/core/repos/product_repo_impl.dart
+++ lib/core/repos/product_repo_impl.dart
@@ -2,12 +2,17 @@ import 'package:dartz/dartz.dart';
 import 'package:fruits_app/core/entities/product_entity.dart';
 import 'package:fruits_app/core/errors/failuer.dart';
 import 'package:fruits_app/core/repos/product_repo.dart';
+import 'package:fruits_app/core/services/database_services.dart';
+import 'package:fruits_app/core/utils/backend_endpoint.dart';
 
-class ProductRepoImpl implements ProductRepo {
+class ProductRepoImpl extends ProductRepo {
+  final DataBaseServices dataBaseServices;
+  ProductRepoImpl(this.dataBaseServices);
   @override
-  Future<Either<Failure, List<ProductEntity>>> getAllProducts() {
-    // TODO: implement getAllProducts
-    throw UnimplementedError();
+  Future<Either<Failure, List<ProductEntity>>> getAllProducts() async {
+    var date = await dataBaseServices.getData(
+        path: BackendEndpoint.getAllProducts) as List<Map<String, dynamic>>;
+    return right(List<ProductEntity>.from(date));
   }
 
   @override
