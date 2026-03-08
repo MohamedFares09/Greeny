18bcedc23024e2445a5b6ba7e1310df78408bc38
Mohamed Fares <mofares010@gmail.com>
2026-03-08 22:01:39 +0200

    add method getData in database_services

--- lib/core/services/database_services.dart
+++ lib/core/services/database_services.dart
@@ -1,5 +1,6 @@
 abstract class DataBaseServices {
   Future<void> addData({ required String path, required Map<String, dynamic> data ,  String? documentId });
   Future<Map<String , dynamic>> getUserData({required String path, required String uid});
+  Future<dynamic> getData({required String path , String? documentId});
   Future<bool> isExisits({required String path , required String documentId});
 }
