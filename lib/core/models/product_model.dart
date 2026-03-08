972bc9e3b9ce9c98dc26c2908e88b0eaeaa9c81a
Mohamed Fares <mofares010@gmail.com>
2026-03-08 22:01:02 +0200

    add variable sellCount in product model

--- lib/core/models/product_model.dart
+++ lib/core/models/product_model.dart
@@ -15,6 +15,7 @@ class ProductModel {
   final bool isOrganic;
   final int numberOfCalories;
   final int unitAmout;
+  final num sellCount;
   final int avgRating = 0;
   final int ratingCount = 0;
   final List<ReviewModel> reviews;
@@ -28,24 +29,26 @@ class ProductModel {
     required this.expirationMonth,
     required this.numberOfCalories,
     required this.unitAmout,
+    required this.sellCount,
     this.isOrganic = false,
     this.urlImage,
     required this.reviews,
   });
-  factory ProductModel.fromEntity(ProductEntity entity) {
+  factory ProductModel.fromJson(Map<String, dynamic> json) {
     return ProductModel(
-      name: entity.name,
-      code: entity.code,
-      description: entity.description,
-      price: entity.price,
-      image: entity.image,
-      isFeatured: entity.isFeatured,
-      urlImage: entity.urlImage,
-      expirationMonth: entity.expirationMonth,
-      numberOfCalories: entity.numberOfCalories,
-      unitAmout: entity.unitAmout,
-      isOrganic: entity.isOrganic,
-      reviews: entity.reviews.map((e) => ReviewModel.fromEntity(e)).toList(),
+      name: json['name'],
+      code: json['code'],
+      description: json['description'],
+      price: json['price'],
+      image: json['image'],
+      isFeatured: json['isFeatured'],
+      urlImage: json['urlImage'],
+      expirationMonth: json['expirationMonth'],
+      numberOfCalories: json['numberOfCalories'],
+      unitAmout: json['unitAmout'],
+      isOrganic: json['isOrganic'],
+      reviews: json['reviews'].map((e) => ReviewModel.fromJson(e)).toList(),
+      sellCount: json['sellCount'],
     );
   }
   toJson() {
@@ -61,6 +64,7 @@ class ProductModel {
       'unitAmout': unitAmout,
       'isOrganic': isOrganic,
       'reviews': reviews.map((e) => e.toJson()).toList(),
+      'sellCount': sellCount,
     };
   }
 }
