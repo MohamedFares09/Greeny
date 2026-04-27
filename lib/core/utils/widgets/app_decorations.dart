8764c9c102d35bc421c4c3e4162cd3825c678ba2
Mohamed Fares <mofares010@gmail.com>
2026-04-28 01:32:32 +0300

    create abstact  app_decoration

---
+++ lib/core/utils/widgets/app_decorations.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+abstract class AppDecoration {
+  static var greyBoxDecoration = BoxDecoration(
+    color: const Color(0x7FF2F3F3),
+    borderRadius: BorderRadius.circular(8.0),
+  );
+}
