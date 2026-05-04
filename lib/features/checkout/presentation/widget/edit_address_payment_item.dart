f077024065b0fab5b94379dc0ff486df08ce0142
Mohamed Fares <mofares010@gmail.com>
2026-05-04 17:41:37 +0300

    create edit address in payment section

---
+++ lib/features/checkout/presentation/widget/edit_address_payment_item.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruits_app/core/utils/app_text_styles.dart';
+import 'package:fruits_app/features/checkout/domain/entity/order_entity.dart';
+
+class EditAddressPaymentItem extends StatelessWidget {
+  const EditAddressPaymentItem({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Text(
+              "عنوان التوصيل",
+              style: AppTextStyles.bold13,
+            ),
+            Spacer(),
+            Icon(
+              Icons.edit,
+              color: Colors.grey,
+              size: 16,
+            ),
+            SizedBox(
+              width: 5,
+            ),
+            Text("تعديل",
+                style: AppTextStyles.semibold13.copyWith(color: Colors.grey)),
+          ],
+        ),
+        SizedBox(
+          height: 8,
+        ),
+        Row(
+          children: [
+            Icon(
+              Icons.location_on,
+              color: Colors.grey,
+              size: 26,
+            ),
+            SizedBox(
+              width: 8,
+            ),
+            Text(
+              "${context.read<OrderEntity>().shippingAddressEntity.toString()}",
+              style: AppTextStyles.regular13,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
